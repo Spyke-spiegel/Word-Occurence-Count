2665ea3167bb6e128c047b9847f8cdd8ffea716c
Florent Ayot <florent.ayot33@gmail.com>
2019-05-27 13:22:30 +0800

    Added the iterating folder function

--- word_count.py
+++ word_count.py
@@ -1,16 +1,25 @@
-filename = './wc_input/words.txt'
-file = open(filename, 'r')
+import os
+
 dic =dict()
-
-for line in file:
-    line = line.rstrip().rstrip('.').rstrip(',')
-    wds = line.split()
-    for w in wds:
-        if w in dic:
-            dic[w] = dic[w] + 1
-        else:
-            dic[w] = 1
-
 result = open('wc_result.txt', 'w+')
-for key in sorted(dic.keys(), key=str.lower):
-    result.write("%s: %s\r\n" % (key, dic[key]))
+folderPath = './wc_input/'
+
+#Iterating over the folder wc_input
+for file in os.listdir(folderPath):
+    filename = os.fsdecode(file)
+    print("file find in the folder Input = " + filename)
+    data = open(folderPath + filename, "r")
+    for line in data:
+        #cleaning the data
+        line = line.rstrip().rstrip('.').rstrip(',')
+        wds = line.split()
+        #counting Words
+        for w in wds:
+            if w in dic:
+                dic[w] = dic[w] + 1
+            else:
+                dic[w] = 1
+    #Ordering the data and writing the result in wc_result.txt
+    result = open('wc_result.txt', 'w+')
+    for key in sorted(dic.keys(), key=str.lower):
+        result.write("%s: %s\r\n" % (key, dic[key]))
